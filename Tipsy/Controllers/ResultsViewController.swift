fee319c87db4e0547a5b03c34641c6ed160f43cb
111 <111>
2020-11-17 21:05:29 +0300

    Link the Results screen in the Main.storyboard with the new ResultsViewController class. link up the necessary IBOutlets and IBActions

--- Tipsy/Controllers/ResultsViewController.swift
+++ Tipsy/Controllers/ResultsViewController.swift
@@ -9,14 +9,18 @@
 import UIKit
 
 class ResultsViewController: UIViewController {
-
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    @IBOutlet weak var settingsLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+    }
+    
     /*
     // MARK: - Navigation
 
